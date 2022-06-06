9593bfceedd4b97bcdd6d1d88d01ccd7fb27c7bc
josemls <jose_9513@hotmail.com>
2022-06-06 18:27:01 +0000

    using puts to display

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,8 @@
 /* using puts */
 #include <stdio.h>
-
+/*
+ * main : "displaying programing is like building a multibilingual puzzle"
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
