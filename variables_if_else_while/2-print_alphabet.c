f2e494e17caf12db6ca1fd140e7b5c35b21a37a2
josemls <jose_9513@hotmail.com>
2022-06-08 16:07:35 +0000

    prints alphabet in lower case

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program that print all lowercase alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+			putchar('\n');
+	return (0);
+}
