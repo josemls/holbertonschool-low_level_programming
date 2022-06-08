0b73789bceb62295f020a9b9d4fec9a02a843d6e
josemls <jose_9513@hotmail.com>
2022-06-08 16:41:33 +0000

    Print all lowercase first and then uppercase

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,17 +9,14 @@ int main(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-
+	{
 		putchar(ch);
-
-		putchar('\n');
+	}
 
 	for (ch = 'A'; ch <= 'Z'; ch++)
-
+	{
 		putchar(ch);
-
-		putchar ('\n');
-
+	}
+	putchar('\n');
 			return (0);
 }
-
