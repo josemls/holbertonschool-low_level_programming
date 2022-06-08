00750dad62f6b9a59b1590df84847a424bc34603
josemls <jose_9513@hotmail.com>
2022-06-08 17:27:22 +0000

    Printing all number form 1 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Printing numbers from 1 to 9
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("%d", i);
+}
