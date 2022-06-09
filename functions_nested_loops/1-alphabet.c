04908cc8df52afab6198981ca466ee19ecbdfd4e
josemls <jose_9513@hotmail.com>
2022-06-09 14:18:02 +0000

    printing alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+}
