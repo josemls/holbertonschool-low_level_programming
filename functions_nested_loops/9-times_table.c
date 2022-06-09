e12666871339ca46e5d02fb39474e879ac02a9c1
josemls <jose_9513@hotmail.com>
2022-06-09 21:47:43 +0000

    prints 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+
+	int num;
+	int multi;
+	int nine;
+
+
+	for (num = 0; num < 10; num++)
+
+	{
+		_putchar('0');
+
+		for (multi = 1; multi < 10; multi++)
+		{
+			_putchar(44);
+			_putchar(32);
+
+			nine = num * multi;
+
+			if (nine < 10)
+				_putchar(32);
+
+			else
+				_putchar((nine / 10) + '0');
+
+			_putchar((nine % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
