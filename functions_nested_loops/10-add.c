3e920b7f78f29855334edc5f921eb76bdff055bf
josemls <jose_9513@hotmail.com>
2022-06-09 21:51:26 +0000

    adding to integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - two integers and returns the result
+ *@a: test
+ *@z: test
+ * Return: Always 0.
+ */
+int add(int a, int z)
+{
+
+	return (a + z);
+}
