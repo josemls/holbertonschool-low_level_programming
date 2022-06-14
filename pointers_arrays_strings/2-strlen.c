739b48098df65beb431be4483f631ff253d9efbd
josemls <jose_9513@hotmail.com>
2022-06-14 22:35:50 +0000

    string lengts

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strlen - return the length of a string
+ *@s: array
+ *Return: n
+ */
+
+int _strlen(char *s)
+{
+
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+	;
+		return (n);
+}
