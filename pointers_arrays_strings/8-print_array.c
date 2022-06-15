26661e78c1cf090b82119f67c196ef8dd1fa6328
josemls <jose_9513@hotmail.com>
2022-06-15 00:46:53 +0000

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n element
+ *@arr: array
+ *@n: - number
+ */
+void print_array(int *arr, int n)
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", arr[i]);
+
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+}
