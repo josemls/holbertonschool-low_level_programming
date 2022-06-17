1d8e2dc407e748ba2b9f3ba96d48259217fa19f8
josemls <jose_9513@hotmail.com>
2022-06-17 20:38:23 +0000

    prefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* *_strspn - length prefix
+* @s: the string
+* @a: the string 
+*
+* Return: pointer to dest.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+	for (j = 0; a[j]; j++)
+	{
+		if (s[i] == accept[j])
+		{
+			break;
+		}
+	}
+		if (!a[j])
+		{
+		break;
+		}
+	}
+	return (i);
+}
