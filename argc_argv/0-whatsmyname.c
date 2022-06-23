99d7c6a571cf9db9a74063b28aac66cff6d4b286
josemls <jose_9513@hotmail.com>
2022-06-23 17:47:06 +0000

    print its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,10 +6,10 @@
  * @argv: characters
  * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
 
