67cd2d4c971608dc0ad224c0324712b4cc7a5db0
josemls <jose_9513@hotmail.com>
2022-06-23 17:56:11 +0000

    numbers of arguments passed to it

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - string
+ * @argc:  number
+ * @argv: characters
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
