73e8eddf96507e12bd004f48e0fb8b8f6684a04f
josemls <jose_9513@hotmail.com>
2022-07-07 01:39:00 +0000

    sum

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,25 +1,25 @@
+#include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-#include <stdio.h>
 /**
-  * print_numbers - print numbers
-  * @n: number of arguments
-  * @separator: string between numbeers
-  */
+ * print_numbers - a function that prints numbers
+ * @separator: separator
+ * @n: num
+ * Return void
+ **/
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list arg;
-	unsigned int i, var;
+	va_list num;
+	unsigned int i;
 
-	va_start(arg, n);
+	va_start(num, n);
 
 	for (i = 0; i < n; i++)
 	{
-		var = va_arg(arg, unsigned int);
-
-		printf("%d", var);
-		if ((i < n - 1) && separator)
+		printf("%d", va_arg(num, int));
+		if (i < (n - 1) && separator)
 			printf("%s", separator);
 	}
 	printf("\n");
-	va_end(arg);
+		va_end(num);
+}
