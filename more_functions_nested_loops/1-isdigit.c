4c3d7c331461ba074772f9a9fbd1dad665d0265c
josemls <jose_9513@hotmail.com>
2022-08-03 17:48:17 +0000

    checking for a digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,15 @@
 
 /**
  * _isdigit - use to check for a digit
- * c: numbers
- * Return : on success 1
+ * @c: numbers
+ * Return: on success 1
  * on error 0
  */
 
 int _isdigit(int c)
 {
+	int c;
+
 	if (c >= 48 && c <= 57)
 		return (1);
 	else
