9abe132583ab81488cce5a0e1292c0f14f22df92
josemls <jose_9513@hotmail.com>
2022-08-03 17:09:58 +0000

    is upper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
 /**
- *_isupper - checks for an uppercase character
- *@c: Char that will be chcecked
- *Return: 1 if char is upper else 0
+ * _isupper -To check if upper
+ * @c: character to print
+ *  Return: On succes 1
+ *  on error 0
  */
 
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
+int _isupper(int c);
 
-	else
-		return (0);
-}
+if (c >= 'A' && c <= 'Z')
+
+	return(1)
+else
+	return(0)
+
