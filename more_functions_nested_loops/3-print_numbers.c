ad1ebf496124f988af2ef7f16385735a7588c1dd
josemls <jose_9513@hotmail.com>
2022-08-04 13:11:31 +0000

    function to count numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - To print numbers
+ * @num : The numbers
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0, num < 10, num++)
+	{
+		_putchar(num)
+	}
+}
+
