0bb641c592db00e41f2f5e01410135547515980e
josemls <jose_9513@hotmail.com>
2022-08-04 14:10:58 +0000

    printing most numbhers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers from 0 to 9
+ *
+ */
+
+void print_most_numbers(void)
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
+	}
