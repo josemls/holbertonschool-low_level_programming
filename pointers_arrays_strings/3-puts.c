3cf0d17df31e6b1062e5cfe3b08ba3a31a0d986a
josemls <jose_9513@hotmail.com>
2022-08-09 22:13:29 +0000

    using puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - To print a string
+ * @str - string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int lays = 0;
+
+	while (str[lays] != '\0')
+	{
+		_putchar(str[lays]);
+		lays++;
+	}
+	_putchar('\n');
+}
