60698ac9289efb8f75b60267dae55c1bf3d0a716
josemls <jose_9513@hotmail.com>
2022-08-18 14:48:24 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat
+ * @dest: destination
+ * @src: source
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int d = 0;
+	int s = 0;
+
+	while ( dest[d] != '\0')
+		{	d++;
+		}
+	for (s = 0; src[s] != '\0'; s++, d++)
+	{	
+		dest[d] = src[s];
+	}
+	dest[d] = '\0';
+	
+	return (dest);
+}
