c6df9a1a7e8988276d3af1435026c5cf9e3935ec
josemls <jose_9513@hotmail.com>
2022-08-18 14:50:34 +0000

    concatenate two strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,14 @@ char *_strcat(char *dest, char *src)
 	int d = 0;
 	int s = 0;
 
-	while ( dest[d] != '\0')
+	while (dest[d] != '\0')
 		{	d++;
 		}
 	for (s = 0; src[s] != '\0'; s++, d++)
-	{	
+	{
 		dest[d] = src[s];
 	}
 	dest[d] = '\0';
-	
+
 	return (dest);
 }
