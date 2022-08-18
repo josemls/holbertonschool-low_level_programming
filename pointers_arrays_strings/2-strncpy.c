9ef6eeec96e887eddc5036139bce264b3ff14613
josemls <jose_9513@hotmail.com>
2022-08-18 18:03:38 +0000

    string copy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncpy - to copie a string
+ * @dest: string 1
+ * @src: string 2
+ * @n: number of characters
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		i++;
+	}
+	dest[i] = '\0';
+
+		return (dest);
+}
+
+
